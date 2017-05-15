d890293f2ab8c82e612e49047074f818c2f78419
ZhangShenao <15201440436@163.com>
2017-05-15 10:56:37 +0800

    JVM内存分配策略

--- src/ch3/AllocateTest.java
+++ src/ch3/AllocateTest.java
@@ -1,5 +1,7 @@
 package ch3;
 
+import org.junit.Test;
+
 /**
  * 
  * <p>Description: JVM的内存分配策略 </p>
@@ -12,10 +14,34 @@ public class AllocateTest {
 	/**
 	 * JVM参数:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
 	 */
+	@Test
 	public void testAllocation(){
 		byte[] allocation1 = new byte[2 * _1MB];
 		byte[] allocation2 = new byte[2 * _1MB];
 		byte[] allocation3 = new byte[2 * _1MB];
 		byte[] allocation4 = new byte[4 * _1MB];	//出现一次Minor GC
+	}
+	
+	/**
+	 * JVM参数:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * -XX:PretenureSizeThreshold=3145728
+	 */
+	@Test
+	public void testPretenureSizeThreshold(){
+		//大对象直接在老年代中分配
+		byte[] allocation = new byte[4 * _1MB];
+	}
+	
+	/**
+	 * JVM参数:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * -XX:MaxTenuringThreshold=1 -XX:+PrintTenuringDistribution
+	 */
+	@Test
+	public void testMaxTenuringThreshold(){
+		byte[] allocation1 = new byte[_1MB / 4];
+		byte[] allocation2 = new byte[4 * _1MB];
+		byte[] allocation3 = new byte[4 * _1MB];
+		allocation3 = null;
+		allocation3 = new byte[4 * _1MB];
 	}
 }
