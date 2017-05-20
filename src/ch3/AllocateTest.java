4c213909442b2904bc28ca4ca30a2e9cf8bcf266
ZhangShenao <15201440436@163.com>
2017-05-20 16:11:48 +0800

    常用垃圾收集器

--- src/ch3/AllocateTest.java
+++ src/ch3/AllocateTest.java
@@ -29,6 +29,7 @@ public class AllocateTest {
 	@Test
 	public void testPretenureSizeThreshold(){
 		//大对象直接在老年代中分配
+		//尽量避免创建大量的"短命大对象"
 		byte[] allocation = new byte[4 * _1MB];
 	}
 	
@@ -43,5 +44,19 @@ public class AllocateTest {
 		byte[] allocation3 = new byte[4 * _1MB];
 		allocation3 = null;
 		allocation3 = new byte[4 * _1MB];
+	}
+	
+	/**
+	 * JVM参数:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * -XX:MaxTenuringThreshold=15 -XX:+PrintTenuringDistribution
+	 */
+	@Test
+	public void testMaxTenuringThreshold1(){
+		byte[] allocation1 = new byte[_1MB / 4];
+		byte[] allocation2 = new byte[_1MB / 4];
+		byte[] allocation3 = new byte[4 * _1MB];
+		byte[] allocation4 = new byte[4 * _1MB];
+		allocation4 = null;
+		allocation4 = new byte[4 * _1MB];
 	}
 }
