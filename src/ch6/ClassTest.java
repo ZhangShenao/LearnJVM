728ef62382555ae999c418c07c44b878e80a1f22
ZhangShenao <15201440436@163.com>
2017-05-22 20:21:29 +0800

    Class字节码结构

---
+++ src/ch6/ClassTest.java
@@ -0,0 +1,15 @@
+package ch6;
+
+/**
+ * 
+ * <p>Description:Class文件格式</p>
+ * @author ZhangShenao
+ * @date 2017年5月22日
+ */
+public class ClassTest {
+	private int m;
+	
+	public int inc(){
+		return ++m;
+	}
+}
